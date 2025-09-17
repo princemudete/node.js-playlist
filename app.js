cd9ecce0503b6622b3fd98c6d92b66750e2eaa3d
princemudete <princemudete@gmail.com>
2025-09-17 15:36:29 +0200

    First commit - setup Node project

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-// READABLE STREAMS
+// PIPE STREAMS
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+var server = http.createServer(function(req, res) {
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+    myReadStream.pipe(res);
+}); 
 
-myReadStream.on('data', function(chunk) {
-    console.log('new chunk received:');
-    myWriteStream.write(chunk);
+server.listen(3000, '127.0.0.1');
+console.log('yo dawgs, now listening to port 3000');
+
+
+// READABLE STREAMS 
+// var http = require('http');
+// var fs = require('fs');
+
+// var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+// var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+
+// myReadStream.on('data', function(chunk) {
+//     console.log('new chunk received:');
+//     myWriteStream.write(chunk);
     // console.log(chunk);
-});
+// });
+
 
 // CREATING A SERVER
 // var http = require('http');
